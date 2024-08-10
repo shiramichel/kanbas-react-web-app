655dc9a80eb1ed25ef8ef979a1a49671105e8ebf
Shira Michel <shiramichel@shiras-mbp.mynetworksettings.com>
2024-08-10 18:29:02 -0400

    attempt to fix fetchProfile

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,5 +1,5 @@
 import * as client from "./client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
@@ -7,6 +7,13 @@ import { setCurrentUser } from "./reducer";
 export default function Profile() {
   const [profile, setProfile] = useState<any>({});
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const signout = async () => {
+    await client.signout();
+    dispatch(setCurrentUser(null));
+    navigate("/Kanbas/Account/Signin");
+  };
+  /*
   const fetchProfile = async () => {
     try {
     const account = await client.profile();
@@ -15,14 +22,19 @@ export default function Profile() {
     navigate("/Kanbas/Account/Signin");
   }
   };
-  const dispatch = useDispatch();
-  const signout = async () => {
-    await client.signout();
-    dispatch(setCurrentUser(null));
-    navigate("/Kanbas/Account/Signin");
-  };
-
   useEffect(() => { fetchProfile(); }, [fetchProfile]);
+  */
+  const fetchProfile = useCallback(async () => {
+    try {
+      const account = await client.profile();
+      setProfile(account);
+    } catch (err: any) {
+      navigate("/Kanbas/Account/Signin");
+    }
+  }, [navigate]);
+  
+  useEffect(() => { fetchProfile(); }, [fetchProfile]);
+  
   return (
     <div className="wd-profile-screen">
       <h1>Profile</h1>
